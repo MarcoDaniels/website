4032cceb7144dc5c74b835bc7e86bb22d4bfc994
marcodaniels <mapma@fynskemedier.dk>
2018-07-23 17:34:21 +0200

    Deploy with Travis

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export const App: React.SFC = () => {
     return (
         <div>
-            We are here!
+            we're building something cool...
         </div>
     )
 }
