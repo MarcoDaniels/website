2af7cbc3791bb200296a438c8c86af8d032bec1a
Marco Martins <marcodanielmartins@gmail.com>
2022-01-26 20:36:38 +0100

    No nix for build

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# MarcoDaniels.com
+# MarcoDaniel's website
 
-[Marco Daniel Martins](https://marcodaniels.com)'s website
+My personal website at [marcodaniels.com](https://marcodaniels.com).
+
+This website and repository consists on several attempts at trying out some cool stuff. 
+
+Wish me luck 🍀
--- default.nix
+++ default.nix
@@ -1,14 +1,16 @@
+# TODO: nix will be used only for development for now
 let
     pkgs = import (import ./nix/pin.nix).nixpkgs {};
 
     yarnPkg = pkgs.yarn2nix-moretea.mkYarnPackage {
         name = "marco-daniels";
         packageJSON = ./package.json;
-        src = null;
+        src = ./.;
         yarnLock = ./yarn.lock;
+        publishBinsFor = ["elm-pages"];
     };
 in pkgs.stdenv.mkDerivation {
-    name = "marco-daniels-website";
+    name = "marco-daniels";
     src = pkgs.lib.cleanSource ./.;
 
     buildInputs = [
@@ -33,7 +35,10 @@ in pkgs.stdenv.mkDerivation {
         elmVersion  = "0.19.1";
     };
 
+    # Error: EROFS: read-only file system
     installPhase = ''
+        export out=/tmp/foo
+        mkdir -p $out
         elm-pages build
     '';
 }
