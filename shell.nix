e1e00bb602c458399ff01162ce7f069b76f2a962
Marco Martins <marco.martins@lego.com>
2022-02-07 21:50:18 +0100

    Setup dev image proxy

--- shell.nix
+++ shell.nix
@@ -1,8 +1,61 @@
 let
   pkgs = import (import ./nix/pin.nix).nixpkgs { };
 
-  devProxy = pkgs.writeShellScriptBin "devProxy" ''
-    echo "🚀 make image API dev proxy";
+  devProxy = pkgs.writeScriptBin "devProxy" ''
+    #!/usr/bin/env node
+    const http = require('http')
+    const url = require('url')
+    const https = require('https')
+
+    const proxy = http.createServer((req, res) => {
+        const request = url.parse(req.url)
+
+        if (request.path.startsWith('/image/api/')) {
+            const path = request.pathname.replace('/image/api', "")
+            const host = (process.env.COCKPIT_BASE_URL).replace("https://", "")
+
+            const options = {
+                host: host,
+                path: "/api/cockpit/image?token=" + process.env.COCKPIT_API_TOKEN + "&src=" + process.env.COCKPIT_BASE_URL + "/storage/uploads" + path + "&" + request.query,
+                method: req.method,
+                headers: {...req.headers, host: host},
+            }
+
+            https.get(options, (backend_res) => {
+                res.writeHead(backend_res.statusCode, backend_res.headers)
+                backend_res.pipe(res, {end: true})
+            })
+        } else {
+            const options = {
+                host: request.hostname,
+                port: 1234,
+                path: request.path,
+                method: req.method,
+                headers: req.headers,
+            }
+
+            const backend_req = http.request(options, (backend_res) => {
+                res.writeHead(backend_res.statusCode, backend_res.headers)
+                backend_res.on('data', (chunk) => {
+                    res.write(chunk)
+                })
+                backend_res.on('end', () => {
+                    res.end()
+                })
+            })
+
+            req.on('data', (chunk) => {
+                backend_req.write(chunk)
+            })
+
+            req.on('end', () => {
+                backend_req.end()
+            })
+        }
+    })
+
+    proxy.listen(8000)
+    console.log(`running dev in http://localhost:8000`)
   '';
 
 in pkgs.mkShell {
