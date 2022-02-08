79f181be4c1cb57c30c76c1d03321856bfd65242
Marco Martins <marco.martins@lego.com>
2022-02-08 21:50:05 +0100

    Home page

--- src/Style/Theme.elm
+++ src/Style/Theme.elm
@@ -5,7 +5,7 @@ import Css.Global
 import Element exposing (Element, ElmElement)
 import Html.Styled as Html
 import Html.Styled.Attributes as Html
-
+import Style.Center exposing (centerStyle)
 
 globalStyle : List (Element msg)
 globalStyle =
@@ -14,19 +14,16 @@ globalStyle =
             [ Css.margin <| Css.px 0
             , Css.padding <| Css.px 0
             , Css.fontFamilies [ "monospace" ]
+            , Css.backgroundColor <| Css.hex "e7e7e7"
             ]
         ]
     ]
 
 
+
 useTheme : List (Element msg) -> ElmElement msg
 useTheme content =
     Html.div
-        [ Html.css
-            [ Css.height <| Css.vh 100
-            , Css.displayFlex
-            , Css.flexDirection Css.column
-            ]
-        ]
+        [ Html.css [ Css.height <| Css.vh 100, centerStyle.column ] ]
         ([ globalStyle, content ] |> List.concat)
         |> Html.toUnstyled
