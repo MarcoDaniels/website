6dffbbc1c9ffc0088c5091524bdb165c048cec1d
Marco Martins <marco.martins@lego.com>
2022-02-13 20:55:47 +0100

    Build lambda handler

--- infrastructure/default.nix
+++ infrastructure/default.nix
@@ -1,12 +1,25 @@
 let
   pkgs = import (import ../nix/pin.nix).nixpkgs { };
 
-  mkLambda =
-    { srcs ? ./elm-srcs.nix, src, name, srcdir, targets, registryDat }:
+  jsHandler = pkgs.writeShellScriptBin "jsHandler" ''
+    echo "const {Elm} = require('./$1')
+    const app = Elm.$1.init()
+
+    exports.handler = (event, context, callback) => {
+        app.ports.inputEvent.send(event)
+        app.ports.outputEvent.subscribe((output) =>
+            callback(null, output)
+        )
+    }
+    " > $out/index.js
+  '';
+
+  mkLambda = { srcs ? ./elm-srcs.nix, src, name, srcdir, targets, registryDat }:
     pkgs.stdenv.mkDerivation {
       inherit name src;
 
-      buildInputs = [ pkgs.elmPackages.elm pkgs.nodePackages.uglify-js ];
+      buildInputs =
+        [ pkgs.elmPackages.elm pkgs.nodePackages.uglify-js jsHandler ];
 
       buildPhase = pkgs.elmPackages.fetchElmDeps {
         elmPackages = import srcs;
@@ -22,10 +35,10 @@ let
         ${pkgs.lib.concatStrings (map (module: ''
           echo "compiling ${elmfile module}"
           elm make ${elmfile module} --output $out/${module}.${extension}
-            echo "minifying ${elmfile module}"
-            uglifyjs $out/${module}.${extension} --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
-                | uglifyjs --mangle --output $out/${module}.${extension}
-            cp ./infrastructure/lambda/index.js $out/index.js
+          echo "minifying ${elmfile module}"
+          uglifyjs $out/${module}.${extension} --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
+            | uglifyjs --mangle --output $out/${module}.${extension}
+          ${jsHandler}/bin/jsHandler ${module}
         '') targets)}
       '';
     };
