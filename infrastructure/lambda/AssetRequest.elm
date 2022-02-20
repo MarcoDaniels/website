b7be20e2a01a4c75c0aed30e1c8d62d53e2a38d8
Marco Martins <marco.martins@lego.com>
2022-02-20 14:47:23 +0100

    Asset request with query & uri

--- infrastructure/lambda/AssetRequest.elm
+++ infrastructure/lambda/AssetRequest.elm
@@ -4,11 +4,31 @@ import CloudWorker exposing (originRequest, toCloudWorker, toRequest)
 
 
 
--- TODO: implement
+-- TODO: access -> get it as flags?
+
+
+access =
+    { token = "TODO:TOKEN", assetURL = "TODO:URI" }
 
 
 main : Program () CloudWorker.Model CloudWorker.Msg
 main =
     originRequest
-        (\request -> request |> toRequest)
+        (\request ->
+            let
+                queryString =
+                    "token="
+                        ++ access.token
+                        ++ "&src="
+                        ++ access.assetURL
+                        ++ String.replace "image/api" "storage/uploads" request.uri
+                        ++ "&"
+                        ++ Maybe.withDefault "" request.querystring
+            in
+            { request
+                | uri = "/api/cockpit/image"
+                , querystring = Just queryString
+            }
+                |> toRequest
+        )
         |> toCloudWorker
