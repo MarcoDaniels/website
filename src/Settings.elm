16192c0de3d6fc1302a1269bc285f12538b2618a
Marco Martins <marco.martins@lego.com>
2022-04-03 13:26:57 +0200

    Update navigation

--- src/Settings.elm
+++ src/Settings.elm
@@ -10,17 +10,28 @@ type alias SiteSettings =
     { title : String, description : String, baseURL : String }
 
 
-type alias NavigationItem =
+type alias LinkItem =
     { title : String, url : String }
 
 
+type alias Navigation =
+    { brand : LinkItem, menu : List LinkItem }
+
+
 type alias Settings =
-    { site : SiteSettings, navigation : List NavigationItem }
+    { site : SiteSettings, navigation : Navigation }
 
 
 settingsData : DataSource.DataSource Settings
 settingsData =
     fetchData (Singleton "marcoDanielsWebsite") settingsDecoder
+
+
+linkValueDecoder : Decoder LinkItem
+linkValueDecoder =
+    Decoder.succeed LinkItem
+        |> Decoder.requiredAt [ "value", "title" ] Decoder.string
+        |> Decoder.requiredAt [ "value", "url" ] Decoder.string
 
 
 settingsDecoder : Decoder Settings
@@ -33,9 +44,11 @@ settingsDecoder =
                 |> Decoder.required "baseURL" Decoder.string
             )
         |> Decoder.required "navigation"
-            (Decoder.list
-                (Decoder.succeed NavigationItem
-                    |> Decoder.requiredAt [ "value", "title" ] Decoder.string
-                    |> Decoder.requiredAt [ "value", "url" ] Decoder.string
-                )
+            (Decoder.succeed Navigation
+                |> Decoder.required "brand"
+                    (Decoder.succeed LinkItem
+                        |> Decoder.required "title" Decoder.string
+                        |> Decoder.required "url" Decoder.string
+                    )
+                |> Decoder.required "menu" (Decoder.list linkValueDecoder)
             )
