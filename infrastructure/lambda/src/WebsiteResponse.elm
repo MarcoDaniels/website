9df14d8d7efbad3ce1e2459f4bf07ffb72beb0c0
Marco Martins <marco.martins@lego.com>
2022-05-17 21:55:51 +0200

    Update security headers list

--- infrastructure/lambda/src/WebsiteResponse.elm
+++ infrastructure/lambda/src/WebsiteResponse.elm
@@ -10,10 +10,20 @@ main =
             \{ response } _ ->
                 response
                     |> withHeaders
-                        [ { key = "strict-transport-security"
-                          , value = "max-age=31536000; includeSubDomains"
+                        [ { key = "x-frame-options", value = "DENY" }
+                        , { key = "x-content-type-options", value = "nosniff" }
+                        , { key = "x-xss-protection", value = "1; mode=block" }
+                        , { key = "referrer-policy", value = "same-origin" }
+                        , { key = "permissions-policy", value = "fullscreen=(self), autoplay=(self)" }
+                        , { key = "strict-transport-security", value = "max-age=31536000; includeSubDomains" }
+                        , { key = "content-security-policy"
+                          , value =
+                                [ "script-src 'self' 'unsafe-inline'; "
+                                , "style-src 'self' 'unsafe-inline'; "
+                                , "object-src 'none'"
+                                ]
+                                    |> String.concat
                           }
-                        , { key = "x-frame-options", value = "DENY" }
                         ]
                     |> toResponse
         }
