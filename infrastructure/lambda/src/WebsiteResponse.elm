b540b7e719db6e202c6bed56caf7535eeefa4d32
Marco Martins <marco.martins@lego.com>
2022-05-26 14:07:44 +0200

    Dev proxy with response headers

--- infrastructure/lambda/src/WebsiteResponse.elm
+++ infrastructure/lambda/src/WebsiteResponse.elm
@@ -1,5 +1,6 @@
-module WebsiteResponse exposing (main)
+module WebsiteResponse exposing (main, websiteResponseHeaders)
 
+import AWS exposing (Header)
 import CloudWorker exposing (cloudWorker, originResponse, toResponse, withHeaders)
 
 
@@ -9,22 +10,26 @@ main =
         { origin =
             \{ response } _ ->
                 response
-                    |> withHeaders
-                        [ { key = "x-frame-options", value = "DENY" }
-                        , { key = "x-content-type-options", value = "nosniff" }
-                        , { key = "x-xss-protection", value = "1; mode=block" }
-                        , { key = "referrer-policy", value = "same-origin" }
-                        , { key = "permissions-policy", value = "fullscreen=(self), autoplay=(self)" }
-                        , { key = "strict-transport-security", value = "max-age=31536000; includeSubDomains" }
-                        , { key = "content-security-policy"
-                          , value =
-                                [ "script-src 'self' 'unsafe-inline'; "
-                                , "style-src 'self' 'unsafe-inline'; "
-                                , "object-src 'none'"
-                                ]
-                                    |> String.concat
-                          }
-                        ]
+                    |> withHeaders websiteResponseHeaders
                     |> toResponse
         }
         |> cloudWorker
+
+
+websiteResponseHeaders : List Header
+websiteResponseHeaders =
+    [ { key = "x-frame-options", value = "DENY" }
+    , { key = "x-content-type-options", value = "nosniff" }
+    , { key = "x-xss-protection", value = "1; mode=block" }
+    , { key = "referrer-policy", value = "same-origin" }
+    , { key = "permissions-policy", value = "fullscreen=(self), autoplay=(self)" }
+    , { key = "strict-transport-security", value = "max-age=31536000; includeSubDomains" }
+    , { key = "content-security-policy"
+      , value =
+            [ "default-src none; "
+            , "style-src 'self' 'unsafe-inline'; "
+            , "object-src 'none'"
+            ]
+                |> String.concat
+      }
+    ]
