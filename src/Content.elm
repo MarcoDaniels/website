045b6ddc0f9ac2c6e22334e573757399b69ac353
Marco Martins <marco.martins@lego.com>
2022-06-15 20:28:12 +0200

    Render markdown to HTML

--- src/Content.elm
+++ src/Content.elm
@@ -1,7 +1,12 @@
 module Content exposing (Asset, Content, ContentData(..), assetDecoder, contentDecoder, contentView)
 
+import Element exposing (Element)
 import Html.Styled as Html
 import Html.Styled.Attributes as Html
+import Markdown.Block as Block
+import Markdown.Html
+import Markdown.Parser as Parser
+import Markdown.Renderer as Render
 import OptimizedDecoder as Decoder exposing (Decoder)
 import OptimizedDecoder.Pipeline as Decoder
 import Style
@@ -73,13 +78,71 @@ contentDecoder =
             )
 
 
+markdownToHTML : String -> List (Element msg)
+markdownToHTML raw =
+    raw
+        |> Parser.parse
+        |> Result.withDefault []
+        |> Render.render
+            { heading =
+                \{ level, children } ->
+                    case level of
+                        Block.H1 ->
+                            Html.h1 [] children
+
+                        Block.H2 ->
+                            Html.h2 [] children
+
+                        Block.H3 ->
+                            Html.h3 [] children
+
+                        Block.H4 ->
+                            Html.h4 [] children
+
+                        Block.H5 ->
+                            Html.h5 [] children
+
+                        Block.H6 ->
+                            Html.h6 [] children
+            , link =
+                \link content ->
+                    case link.title of
+                        Just title ->
+                            Html.a [ Html.href link.destination, Html.title title ] content
+
+                        Nothing ->
+                            Html.a [ Html.href link.destination ] content
+            , paragraph = Html.p []
+            , hardLineBreak = Html.br [] []
+            , blockQuote = Html.blockquote []
+            , strong = \children -> Html.strong [] children
+            , emphasis = \children -> Html.em [] children
+            , strikethrough = \children -> Html.del [] children
+            , codeSpan = \content -> Html.code [] [ Html.text content ]
+            , image = \_ -> Html.div [] []
+            , text = Html.text
+            , orderedList = \_ _ -> Html.div [] []
+            , unorderedList = \_ -> Html.div [] []
+            , html = Markdown.Html.oneOf []
+            , codeBlock = \_ -> Html.div [] []
+            , thematicBreak = Html.hr [] []
+            , table = Html.table []
+            , tableHeader = Html.thead []
+            , tableBody = Html.tbody []
+            , tableRow = Html.tr []
+            , tableHeaderCell = \_ _ -> Html.div [] []
+            , tableCell = \_ _ -> Html.div [] []
+            }
+        |> Result.withDefault []
+
+
 contentView : List Content -> List (Html.Html msg)
 contentView =
     List.map
         (\contentData ->
             case contentData.value of
-                ContentMarkdown string ->
-                    Html.div [ Html.css [ Style.gap.medium ] ] [ Html.text string ]
+                ContentMarkdown markdown ->
+                    Html.div [ Html.css [ Style.gap.medium ] ] (markdownToHTML markdown)
 
                 ContentAsset asset ->
                     Html.img [ Html.alt asset.title, Html.width 200, Html.src (toImageAPI asset.path 300) ] []
