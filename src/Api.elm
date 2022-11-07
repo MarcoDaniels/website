957b7f55ea79893995b722ed49f93bc80e05e635
Marco Martins <marco.martins@lego.com>
2022-11-07 20:18:39 +0100

    Add robots.txt

--- src/Api.elm
+++ src/Api.elm
@@ -4,11 +4,26 @@ import ApiRoute
 import DataSource exposing (DataSource)
 import Html as ElmHtml
 import Route exposing (Route)
+import Settings exposing (settingsData)
 
 
 routes :
     DataSource (List Route)
     -> (ElmHtml.Html Never -> String)
     -> List (ApiRoute.ApiRoute ApiRoute.Response)
-routes getStaticRoutes htmlToString =
-    []
+routes getStaticRoutes _ =
+    [ ApiRoute.succeed
+        (settingsData
+            |> DataSource.map
+                (\settings ->
+                    { body =
+                        [ "User-agent: *"
+                        , "Host:" ++ settings.site.baseURL
+                        ]
+                            |> String.join "\n"
+                    }
+                )
+        )
+        |> ApiRoute.literal "robots.txt"
+        |> ApiRoute.single
+    ]
