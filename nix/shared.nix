e0e20545190fb48aec2d131bcc9fbfc91c215525
Marco Martins <marcodanielmartins@gmail.com>
2023-10-29 22:03:52 +0100

    Use gleam proxy

--- nix/shared.nix
+++ nix/shared.nix
@@ -1,8 +1,8 @@
 let
   pkgs = import (fetchTarball {
-    name = "nixpkgs-23.05-darwin";
-    url = "https://github.com/NixOS/nixpkgs/archive/fc541b860a28.tar.gz";
-    sha256 = "0929i9d331zgv86imvsdzyfsrnr7zwhb7sdh8sw5zzsp7qsxycja";
+    name = "nixpkgs-23.05-darwin-2023-10-05";
+    url = "https://github.com/NixOS/nixpkgs/archive/1e9c7c0203be.tar.gz";
+    sha256 = "10qbybc9k3dj1xap9n0i3z7pc3svzwhclgsyfzzsf8cfh8l518pn";
   }) { };
 
   jsHandler = pkgs.writeShellScriptBin "jsHandler" ''
