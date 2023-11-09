3214614fa846d19e54ac6cfdb4219169d58cf837
Marco Martins <marcodanielmartins@gmail.com>
2023-11-09 21:24:02 +0100

    Run build debug first

--- buildWebsite.nix
+++ buildWebsite.nix
@@ -2,7 +2,8 @@ let
   pkgs = (import ./nix/shared.nix).pkgs;
 
   buildWebsite = pkgs.writeScriptBin "buildWebsite" ''
-    ${pkgs.elmPackages.elm-pages}/bin/elm-pages build
+    ${pkgs.elmPackages.elm-pages}/bin/elm-pages build --debug
+    ${pkgs.elmPackages.elm-pages}/bin/elm-pages build || true
   '';
 
 in pkgs.mkShell {
