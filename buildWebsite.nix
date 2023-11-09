82d42949287ee8d317603fa190d8df263ca467ab
Marco Martins <marcodanielmartins@gmail.com>
2023-11-09 21:40:03 +0100

    Validate build

--- buildWebsite.nix
+++ buildWebsite.nix
@@ -1,9 +1,17 @@
 let
   pkgs = (import ./nix/shared.nix).pkgs;
 
+  buildValidate = pkgs.writeScriptBin "buildValidate" ''
+    if [ ! -f ./dist/elm.js ]; then
+        echo "Elm build not found"
+        exit 1
+    fi
+  '';
+
   buildWebsite = pkgs.writeScriptBin "buildWebsite" ''
     ${pkgs.elmPackages.elm-pages}/bin/elm-pages build --debug
     ${pkgs.elmPackages.elm-pages}/bin/elm-pages build || true
+    ${buildValidate}/bin/buildValidate
   '';
 
 in pkgs.mkShell {
